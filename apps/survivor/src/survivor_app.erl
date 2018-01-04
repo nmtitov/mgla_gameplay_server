6b12d1232ccab83e8d280c5278b13b8e3c6de2fb
Nikita Titov <nmtitov@yandex.ru>
2018-01-04 19:47:23 +0200

    Auto indent lines

--- apps/survivor/src/survivor_app.erl
+++ apps/survivor/src/survivor_app.erl
@@ -5,20 +5,20 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    Routes = [ {
-        '_',
-        [
-            {"/", http_root_handler, []},
-            {"/websocket", ws_handler, []}
-        ]
-    } ],
-    Dispatch = cowboy_router:compile(Routes),
+  Routes = [ {
+    '_',
+    [
+      {"/", http_root_handler, []},
+      {"/websocket", ws_handler, []}
+    ]
+  } ],
+  Dispatch = cowboy_router:compile(Routes),
 
-    TransOpts = [{ip, {0,0,0,0}}, {port, 8000}],
-    ProtoOpts = #{env => #{dispatch => Dispatch}},
-    
-    {ok, _} = cowboy:start_clear(survivor, TransOpts, ProtoOpts),
-    survivor_sup:start_link().
+  TransOpts = [{ip, {0,0,0,0}}, {port, 8000}],
+  ProtoOpts = #{env => #{dispatch => Dispatch}},
+
+  {ok, _} = cowboy:start_clear(survivor, TransOpts, ProtoOpts),
+  survivor_sup:start_link().
 
 stop(_State) ->
-    ok.
+  ok.
