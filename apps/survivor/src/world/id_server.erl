e142d3c20196ae22bb1ac8cd099d769d66329794
Nikita Titov <nmtitov@yandex.ru>
2018-01-07 21:22:56 +0200

    Delete comments

--- apps/survivor/src/world/id_server.erl
+++ apps/survivor/src/world/id_server.erl
@@ -1,21 +1,10 @@
-%%%-------------------------------------------------------------------
-%%% @author nt
-%%% @copyright (C) 2018, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 04. Jan 2018 19:53
-%%%-------------------------------------------------------------------
 -module(id_server).
 -author("nt").
 
 -behaviour(gen_server).
 
-%% API
--export([start_link/0,
-  create_id/0]).
+-export([start_link/0, id/0]).
 
-%% gen_server callbacks
 -export([init/1,
   handle_call/3,
   handle_cast/2,
@@ -25,52 +14,20 @@
 
 -define(SERVER, ?MODULE).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
+id() ->
+  gen_server:call(?SERVER, id).
 
-create_id() ->
-  gen_server:call(?SERVER, create_id).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
 -spec(start_link() ->
   {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 -spec(init(Args :: term()) ->
   {ok, State :: integer()} | {ok, State :: integer(), timeout() | hibernate} |
   {stop, Reason :: term()} | ignore).
 init([]) ->
   {ok, 0}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @end
-%%--------------------------------------------------------------------
 -spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
     State :: integer()) ->
   {reply, Reply :: term(), NewState :: integer()} |
@@ -79,19 +36,12 @@ init([]) ->
   {noreply, NewState :: integer(), timeout() | hibernate} |
   {stop, Reason :: term(), Reply :: term(), NewState :: integer()} |
   {stop, Reason :: term(), NewState :: integer()}).
-handle_call(create_id, _From, State) ->
+handle_call(id, _From, State) ->
   NewState = State + 1,
   {reply, {ok, State}, NewState};
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @end
-%%--------------------------------------------------------------------
 -spec(handle_cast(Request :: term(), State :: integer()) ->
   {noreply, NewState :: integer()} |
   {noreply, NewState :: integer(), timeout() | hibernate} |
@@ -99,16 +49,6 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Request, State) ->
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 -spec(handle_info(Info :: timeout() | term(), State :: integer()) ->
   {noreply, NewState :: integer()} |
   {noreply, NewState :: integer(), timeout() | hibernate} |
@@ -116,36 +56,13 @@ handle_cast(_Request, State) ->
 handle_info(_Info, State) ->
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 -spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
     State :: integer()) -> term()).
 terminate(_Reason, _State) ->
   ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
 -spec(code_change(OldVsn :: term() | {down, term()}, State :: integer(),
     Extra :: term()) ->
   {ok, NewState :: integer()} | {error, Reason :: term()}).
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
