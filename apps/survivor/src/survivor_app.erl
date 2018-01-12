cb3f24448d5fea7b8b86ced99d9204a401618890
Nikita Titov <nmtitov@yandex.ru>
2018-01-12 19:58:14 +0200

    Listen on 8080

--- apps/survivor/src/survivor_app.erl
+++ apps/survivor/src/survivor_app.erl
@@ -14,7 +14,7 @@ start(_StartType, _StartArgs) ->
   } ],
   Dispatch = cowboy_router:compile(Routes),
 
-  TransOpts = [{ip, {0,0,0,0}}, {port, 8000}],
+  TransOpts = [{ip, {0,0,0,0}}, {port, 8080}],
   ProtoOpts = #{env => #{dispatch => Dispatch}},
 
   {ok, _} = cowboy:start_clear(survivor, TransOpts, ProtoOpts),
