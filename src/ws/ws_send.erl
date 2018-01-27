fdb15c66f01335be1b4c86b7855040c0169f4653
Nikita Titov <nmtitov@yandex.ru>
2018-01-27 21:40:41 +0200

    Rearrange functions

--- src/ws/ws_send.erl
+++ src/ws/ws_send.erl
@@ -1,24 +1,7 @@
 -module(ws_send).
 -author("nt").
 -include("../../include/avatar.hrl").
--export([update_message/3, enter_message/1, leave_message/1]).
-
--spec update_message(Id, Point, State) -> Message when Id :: id_server:id(), Point :: point:point(), State :: avatar_state(), Message :: jsx:json_text().
-update_message(Id, {X, Y}, State) ->
-  jsx:encode(#{
-    type => teleport,
-    body => #{
-      id => Id,
-      point => #{
-        x => X,
-        y => Y
-      },
-      new_state => if
-                     State == undefined -> null;
-                     true -> State
-                   end
-    }
-  }).
+-export([enter_message/1, leave_message/1, update_message/3]).
 
 -spec enter_message(Id) -> Message when Id :: id_server:id(), Message :: jsx:json_text().
 enter_message(Id) ->
@@ -35,5 +18,22 @@ leave_message(Id) ->
     type => leave,
     body => #{
       id => Id
+    }
+  }).
+
+-spec update_message(Id, Point, State) -> Message when Id :: id_server:id(), Point :: point:point(), State :: avatar_state(), Message :: jsx:json_text().
+update_message(Id, {X, Y}, State) ->
+  jsx:encode(#{
+    type => teleport,
+    body => #{
+      id => Id,
+      point => #{
+        x => X,
+        y => Y
+      },
+      new_state => if
+                     State == undefined -> null;
+                     true -> State
+                   end
     }
   }).
