64ae6c54c89fda47cd0358f89315ea37359979f5
Nikita Titov <nmtitov@yandex.ru>
2018-01-30 15:07:26 +0200

    Validate vec functions

--- src/math/vec.erl
+++ src/math/vec.erl
@@ -1,36 +1,51 @@
 -module(vec).
 -author("nt").
 
--export([vec_from_points/2, magnitude/1, unit/1, add/2, subtract/2, scale/2, scalar_multiply/2]).
+-export([is_vec/1, vec_from_points/2, magnitude/1, unit/1, add/2, subtract/2, scale/2, scalar_multiply/2]).
 
 -type vec() :: {number(), number()}.
 -export_type([vec/0]).
 
--spec vec_from_points(A, B) -> C when A :: point:point(), B :: point:point(), C :: vec:vec().
-vec_from_points({X1, Y1}, {X2, Y2}) ->
+-spec is_vec(V :: vec()) -> boolean().
+is_vec({X, Y}) when is_number(X), is_number(Y) -> true;
+is_vec(_) -> false.
+
+-spec vec_from_points(A :: point:point(), B :: point:point()) -> vec().
+vec_from_points({X1, Y1} = A, {X2, Y2} = B) ->
+  case is_vec(A) of false -> error(badarg); _ -> ok end,
+  case is_vec(B) of false -> error(badarg); _ -> ok end,
   {X2 - X1, Y2 - Y1}.
 
--spec magnitude(vec:vec()) -> float().
-magnitude({X, Y}) ->
+-spec magnitude(V :: vec()) -> float().
+magnitude({X, Y} = V) ->
+  case is_vec(V) of false -> error(badarg); _ -> ok end,
   math:sqrt(X * X + Y * Y).
 
--spec unit(vec:vec()) -> {float(), float()}.
+-spec unit(V :: vec()) -> vec().
 unit({X, Y} = V) ->
+  case is_vec(V) of false -> error(badarg); _ -> ok end,
   L = magnitude(V),
   {X / L, Y / L}.
 
--spec add(vec:vec(), vec:vec()) -> vec:vec().
-add({X1, Y1}, {X2, Y2}) ->
+-spec add(A :: vec(), B :: vec()) -> vec().
+add({X1, Y1} = A, {X2, Y2} = B) ->
+  case is_vec(A) of false -> error(badarg); _ -> ok end,
+  case is_vec(B) of false -> error(badarg); _ -> ok end,
   {X1 + X2, Y1 + Y2}.
 
--spec subtract(vec:vec(), vec:vec()) -> vec:vec().
-subtract({X1, Y1}, {X2, Y2}) ->
+-spec subtract(A :: vec(), B :: vec()) -> vec().
+subtract({X1, Y1} = A, {X2, Y2} = B) ->
+  case is_vec(A) of false -> error(badarg); _ -> ok end,
+  case is_vec(B) of false -> error(badarg); _ -> ok end,
   {X1 - X2, Y1 - Y2}.
 
--spec scale(vec:vec(), number()) -> vec:vec().
-scale({X, Y}, K) ->
+-spec scale(V :: vec(), K :: number()) -> vec().
+scale({X, Y} = V, K) ->
+  case is_vec(V) of false -> error(badarg); _ -> ok end,
   {X * K, Y * K}.
 
--spec scalar_multiply(vec:vec(), vec:vec()) -> vec:vec().
-scalar_multiply({X1, Y1}, {X2, Y2}) ->
+-spec scalar_multiply(A :: vec(), B :: vec()) -> vec().
+scalar_multiply({X1, Y1} = A, {X2, Y2} = B) ->
+  case is_vec(A) of false -> error(badarg); _ -> ok end,
+  case is_vec(B) of false -> error(badarg); _ -> ok end,
   {X1 * X2, Y1 * Y2}.
