08b7855bac64b0e0e90f1d952912e6fee753eaaa
Nikita Titov <nmtitov@yandex.ru>
2018-01-30 10:26:55 +0200

    Fix spec style

--- src/math/point.erl
+++ src/math/point.erl
@@ -10,10 +10,10 @@
 is_point({X, Y}) when is_number(X), is_number(Y) -> true;
 is_point(_)                                      -> false.
 
--spec distance(A, B) -> C when A :: point(), B :: point(), C :: float().
+-spec distance(A :: point(), B :: point()) -> float().
 distance({X1, Y1}, {X2, Y2}) ->
   math:sqrt(math:pow((X2 - X1), 2) + math:pow(Y2 - Y1, 2)).
 
--spec translate(A, B) -> C when A :: point(), B :: point(), C :: point().
+-spec translate(A :: point(), B :: point()) -> point().
 translate({X1, Y1}, {X2, Y2}) ->
   {X1 + X2, Y1 + Y2}.
