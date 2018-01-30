09b2b5636d9fcf7f93e0e1f524138f9dd1e0e0a3
Nikita Titov <nmtitov@yandex.ru>
2018-01-30 11:15:17 +0200

    Add eunit tests

--- src/proper/my_sort.erl
+++ src/proper/my_sort.erl
@@ -11,7 +11,18 @@
 
 %% API
 -export([sort/1]).
+-include_lib("eunit/include/eunit.hrl").
 
 -spec sort([T]) -> [T].
 sort([])    -> [];
 sort([H|T]) -> sort([X || X <- T, X < H]) ++ [H] ++ sort([X || X <- T, H < X]).
+
+sort_test_() ->
+  [test_zero(), test_two(), test_four()].
+
+test_zero() ->
+  ?_assertEqual([], sort([])). % notice underscore
+test_two() ->
+  [?_assertEqual([17,42], sort([X,Y])) || {X,Y} <- [{17,42}, {42,17}]].
+test_four() ->
+  [?_assertEqual([1,2,3,4], sort([3,1,4,2]))].
