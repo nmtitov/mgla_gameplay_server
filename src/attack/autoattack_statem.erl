5cfb4ad0b51e2a1439cf4ab3f283d96f212f3dfa
Nikita Titov <nmtitov@yandex.ru>
2018-02-04 15:12:44 +0200

    Fix implementation issues

--- src/attack/autoattack_statem.erl
+++ src/attack/autoattack_statem.erl
@@ -24,6 +24,7 @@ start_link(Id) ->
   gen_statem:start_link(autoattack_statem, [Id], []).
 
 set_target(TargetId, Id) ->
+  lager:info("~p:~p(~p, ~p)", [?MODULE, ?FUNCTION_NAME, TargetId, Id]),
   {ok, _} = gproc_tools:statem_call(name(Id), {set_target,TargetId}).
 
 update(Dt, Id) ->
@@ -37,14 +38,14 @@ init([Id]) ->
   gproc:reg(name(Id)),
   Data = #{
     id => Id,
-    init_cd => 5000,
+    init_cd => 5,
     cd => 0,
     target => undefined
   },
   {ok,ready,Data}.
 
 callback_mode() ->
-  state_functions.
+  [state_functions, state_enter].
 
 terminate(_Reason = M, State, #{id := Id} = Data) ->
   lager:info("~p:~p(~p, ~p, ~p)", [?MODULE, ?FUNCTION_NAME, M, State, Data]),
@@ -57,36 +58,51 @@ code_change(_Vsn, State, Data, _Extra) ->
 
 %%% State
 
-cd({call,_}, {update,Dt}, #{cd := Cd} = D) ->
+cd(enter, OldState, D) ->
+  lager:info("~p:~p(~p, ~p, ~p)", [?MODULE, ?FUNCTION_NAME, enter, OldState, D]),
+  {keep_state,D};
+
+cd({call,From}, {update,Dt}, #{cd := Cd} = D) ->
   Cd2 = Cd - Dt,
   D2 = D#{cd := Cd2},
-  case Cd2 < 0 of
-    true  -> {next_state,ready,D2};
-    false -> {keep_state,D2}
+  case Cd2 > 0 of
+    true  -> {keep_state,D2,[{reply,From,{ok,D2}}]};
+    false -> {next_state,ready,D2,[{reply,From,{ok,D2}}]}
   end;
-cd({call,_}, {set_target, T}, D) ->
+cd({call,From} = E, {set_target, T} = M, D) ->
+  lager:info("~p:~p(~p, ~p)", [?MODULE, ?FUNCTION_NAME, E, M]),
   D2 = D#{target := T},
-  {keep_state,D2}.
+  {keep_state,D2,[{reply,From,{ok,D2}}]}.
 
 
-ready({call,_}, {update,_}, #{init_cd := InitCd, target := T} = D) when T =/= undefined ->
+ready(enter, OldState, D) ->
+  lager:info("~p:~p(~p, ~p, ~p)", [?MODULE, ?FUNCTION_NAME, enter, OldState, D]),
+  {keep_state,D};
+
+ready({call,From} = E, {update,_} = M, #{init_cd := InitCd, target := T} = D) when T =/= undefined ->
+  lager:info("~p:~p(~p, ~p)", [?MODULE, ?FUNCTION_NAME, E, M]),
   D2 = D#{cd := InitCd},
   do_attack(T),
-  {next_state,cd,D2};
+  {next_state,cd,D2,[{reply,From,{ok,D2}}]};
 
-ready(_, {set_target,undefined = T}, D) ->
-  D2 = D#{target := T},
-  {keep_state,D2};
+ready({call,From}, {update,_}, D) ->
+  {keep_state,D,[{reply,From,{ok,D}}]};
 
-ready(_, {set_target,T}, #{init_cd := InitCd} = D) ->
+ready({call,From}, {set_target,undefined} = M, D) ->
+  lager:info("~p", [M]),
+  D2 = D#{target := undefined},
+  {keep_state,D2,[{reply,From,{ok,D2}}]};
+
+ready({call,From}, {set_target,T} = M, #{init_cd := InitCd} = D) ->
+  lager:info("~p", [M]),
   D2 = D#{cd := InitCd, target := T},
   do_attack(T),
-  {next_state,cd,D2}.
+  {next_state,cd,D2,[{reply,From,{ok,D2}}]}.
 
 
 %% Actions
 
 do_attack(TargetId) when TargetId =/= undefined ->
   lager:info("Attacking #~p", [TargetId]),
-  Damage = 1,
+  Damage = 10,
   {ok,_} = av_sapi:subtract_health(Damage, TargetId).
