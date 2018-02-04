dec0870ea66fb59ecfbac9ac065ea2c763bcac5a
Nikita Titov <nmtitov@yandex.ru>
2018-02-04 12:07:15 +0200

    Create attack group

--- src/av/data/av_attack.erl
+++ src/av/data/av_attack.erl
@@ -29,54 +29,64 @@
 -export_type([state/0, target/0]).
 
 -spec get_attack_default_cooldown(D :: av:data()) -> X :: number().
-get_attack_default_cooldown(#{attack_default_cooldown := X}) -> X.
+get_attack_default_cooldown(#{attack := #{default_cooldown := X}}) -> X.
 
 -spec set_attack_default_cooldown(X :: number(), D :: av:data()) -> av:data().
-set_attack_default_cooldown(X, D) ->
+set_attack_default_cooldown(X, #{attack := N} = D) ->
   case X < 0 of true -> error(badarg); _ -> ok end,
   D#{
-    attack_default_cooldown := X
+    attack := N#{
+      default_cooldown := X
+    }
   }.
 
 -spec get_attack_cooldown(D :: av:data()) -> X :: number().
-get_attack_cooldown(#{attack_cooldown := X}) -> X.
+get_attack_cooldown(#{attack := #{cooldown := X}}) -> X.
 
 -spec set_attack_cooldown(X :: number(), D :: av:data()) -> av:data().
-set_attack_cooldown(X, #{attack_default_cooldown := DefaultCooldown, attack_cooldown := X} = D) when is_number(X) ->
+set_attack_cooldown(X, #{attack := #{default_cooldown := DefaultCooldown, cooldown := X} = N} = D) when is_number(X) ->
   D#{
-    attack_cooldown := if
-                         X > DefaultCooldown -> DefaultCooldown;
-                         X < 0 -> 0;
-                         true -> X
-                       end
+    attack := N#{
+      cooldown := if
+         X > DefaultCooldown -> DefaultCooldown;
+         X < 0 -> 0;
+         true -> X
+       end
+    }
   }.
 
 -spec get_attack_state_value(D) -> X when D :: av:data(), X :: state().
-get_attack_state_value(#{attack_state := #{value := X}}) -> X.
+get_attack_state_value(#{attack := #{state := #{value := X}}}) -> X.
 
 -spec set_attack_state_value(X :: state(), D :: av:data()) -> av:data().
-set_attack_state_value(X, #{attack_state := N} = D) when is_atom(X) ->
+set_attack_state_value(X, #{attack := #{state := NN} = N} = D) when is_atom(X) ->
   D#{
-    attack_state := N#{
-      value := X,
-      update := true
+    attack := N#{
+      state := NN #{
+        value := X,
+        update := true
+      }
     }
   }.
 
 -spec get_attack_state_update(D :: av:data()) -> boolean().
-get_attack_state_update(#{attack_state := #{update := X}}) -> X.
+get_attack_state_update(#{attack := #{state := #{update := X}}}) -> X.
 
 -spec get_attack_target(D :: av:data()) -> target().
-get_attack_target(#{attack_target := X}) -> X.
+get_attack_target(#{attack := #{target := X}}) -> X.
 
 -spec set_attack_target(X :: target(), D :: av:data()) -> av:data().
-set_attack_target(X, D) when is_number(X) ->
+set_attack_target(X, #{attack := N} = D) when is_number(X) ->
   D#{
-    attack_target := X
+    attack := N#{
+      target := X
+    }
   }.
 
 -spec clear_attack_target(D :: av:data()) -> av:data().
-clear_attack_target(D) ->
+clear_attack_target(#{attack := N} = D) ->
   D#{
-    attack_target := undefined
+    attack := N#{
+      target := undefined
+    }
   }.
