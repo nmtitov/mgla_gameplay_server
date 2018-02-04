83134ce3746c6f92ddcaa85dc0a05362db6cb20e
Nikita Titov <nmtitov@yandex.ru>
2018-02-04 11:32:33 +0200

    Create av_sdapi

--- src/av/av_sapi.erl
+++ src/av/av_sapi.erl
@@ -8,7 +8,6 @@
 %%%-------------------------------------------------------------------
 -module(av_sapi).
 -author("nt").
-
 -include("../../include/block.hrl").
 
 %% API
@@ -17,23 +16,15 @@
 
   get_data/1,
 
-  get_position/1,
-  set_position/2,
-
   add_health/2,
   subtract_health/2,
 
   add_mana/2,
   subtract_mana/2,
 
-  get_state/1,
-  set_state/2,
-
-  move/4,
-
-  is_dirty/1,
   update/4,
   broadcast_update/1,
+
   clear_update_flags/1
 ]).
 
@@ -41,17 +32,11 @@
 handle_click(Id, Point, AvatarId) ->
   gproc_tools:cast(av_srv:name(Id), {handle_click, Point, AvatarId}).
 
+
 -spec get_data(Id :: id_server:id()) -> {ok, av:data()} | gproc_tools:not_found().
 get_data(Id) ->
   gproc_tools:call(av_srv:name(Id), get_data).
 
--spec get_position(Id :: id_server:id()) -> {ok, point:point()} | gproc_tools:not_found().
-get_position(Id) ->
-  gproc_tools:call(av_srv:name(Id), get_position).
-
--spec set_position(P :: point:point(), Id :: id_server:id()) -> ok | gproc_tools:not_found().
-set_position(P, Id) ->
-  gproc_tools:cast(av_srv:name(Id), {set_position, P}).
 
 -spec add_health(X :: number(), Id :: id_server:id()) -> ok | gproc_tools:not_found().
 add_health(X, Id) ->
@@ -61,6 +46,7 @@ add_health(X, Id) ->
 subtract_health(X, Id) ->
   gproc_tools:call(av_srv:name(Id), {subtract_health, X}).
 
+
 -spec add_mana(X :: number(), Id :: id_server:id()) -> ok | gproc_tools:not_found().
 add_mana(X, Id) ->
   gproc_tools:cast(av_srv:name(Id), {add_mana, X}).
@@ -69,25 +55,6 @@ add_mana(X, Id) ->
 subtract_mana(X, Id) ->
   gproc_tools:cast(av_srv:name(Id), {subtract_mana, X}).
 
--spec get_state(Id :: id_server:id()) -> {ok, av:state()} | gproc_tools:not_found().
-get_state(Id) ->
-  gproc_tools:call(av_srv:name(Id), get_state).
-
--spec set_state(X :: av:state(), Id :: id_server:id()) -> ok | gproc_tools:not_found().
-set_state(X, Id) ->
-  gproc_tools:cast(av_srv:name(Id), {set_state, X}).
-
--spec move(Dt :: float(), MapRect :: rect:rect(), Blocks :: [block()], Id :: id_server:id()) -> {ok, av:data()} | gproc_tools:not_found().
-move(Dt, MapRect, Blocks, Id) ->
-  gproc_tools:call(av_srv:name(Id), {move, Dt, MapRect, Blocks}).
-
--spec is_dirty(Id :: id_server:id()) -> {ok, boolean()} | gproc_tools:not_found().
-is_dirty(Id) ->
-  gproc_tools:call(av_srv:name(Id), is_dirty).
-
--spec clear_update_flags(Id :: id_server:id()) -> {ok, av:data()} | gproc_tools:not_found().
-clear_update_flags(Id) ->
-  gproc_tools:call(av_srv:name(Id), clear_update_flags).
 
 -spec update(Dt :: float(), MapRect :: rect:rect(), Blocks :: [block()], Id :: id_server:id()) -> {ok, _} | gproc_tools:not_found().
 update(Dt, MapRect, Blocks, Id) ->
@@ -96,3 +63,7 @@ update(Dt, MapRect, Blocks, Id) ->
 -spec broadcast_update(Id :: id_server:id()) -> {ok, _} | gproc_tools:not_found().
 broadcast_update(Id) ->
   gproc_tools:call(av_srv:name(Id), broadcast_update).
+
+-spec clear_update_flags(Id :: id_server:id()) -> {ok, av:data()} | gproc_tools:not_found().
+clear_update_flags(Id) ->
+  gproc_tools:call(av_srv:name(Id), clear_update_flags).
---
+++ src/av/av_sdapi.erl
@@ -0,0 +1,51 @@
+%%%-------------------------------------------------------------------
+%%% @author nt
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 04. Feb 2018 11:25
+%%%-------------------------------------------------------------------
+-module(av_sdapi).
+-author("nt").
+-include("../../include/block.hrl").
+
+%% API
+-export([
+  get_position/1,
+  set_position/2,
+
+  get_state/1,
+  set_state/2,
+
+  move/4,
+
+  is_dirty/1
+]).
+
+-spec get_position(Id :: id_server:id()) -> {ok, point:point()} | gproc_tools:not_found().
+get_position(Id) ->
+  gproc_tools:call(av_srv:name(Id), get_position).
+
+-spec set_position(P :: point:point(), Id :: id_server:id()) -> ok | gproc_tools:not_found().
+set_position(P, Id) ->
+  gproc_tools:cast(av_srv:name(Id), {set_position, P}).
+
+
+-spec get_state(Id :: id_server:id()) -> {ok, av:state()} | gproc_tools:not_found().
+get_state(Id) ->
+  gproc_tools:call(av_srv:name(Id), get_state).
+
+-spec set_state(X :: av:state(), Id :: id_server:id()) -> ok | gproc_tools:not_found().
+set_state(X, Id) ->
+  gproc_tools:cast(av_srv:name(Id), {set_state, X}).
+
+
+-spec move(Dt :: float(), MapRect :: rect:rect(), Blocks :: [block()], Id :: id_server:id()) -> {ok, av:data()} | gproc_tools:not_found().
+move(Dt, MapRect, Blocks, Id) ->
+  gproc_tools:call(av_srv:name(Id), {move, Dt, MapRect, Blocks}).
+
+
+-spec is_dirty(Id :: id_server:id()) -> {ok, boolean()} | gproc_tools:not_found().
+is_dirty(Id) ->
+  gproc_tools:call(av_srv:name(Id), is_dirty).
