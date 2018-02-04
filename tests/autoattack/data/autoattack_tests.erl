81c97b17e4430ac6b31b49f331a29e15a46327ec
Nikita Titov <nmtitov@yandex.ru>
2018-02-04 21:01:49 +0200

    More tests

--- tests/autoattack/data/autoattack_tests.erl
+++ tests/autoattack/data/autoattack_tests.erl
@@ -19,6 +19,40 @@ new_test_() ->
     ?_assertEqual(undefined, autoattack:get_target(D))
   ].
 
+get_id_test_() ->
+  D = autoattack:new(0, 5),
+  [
+    ?_assertEqual(0, autoattack:get_id(D))
+  ].
+
+get_cooldown_test_() ->
+  D = autoattack:new(0, 5),
+  [
+    ?_assertEqual(5, autoattack:get_cooldown(D))
+  ].
+
+get_time_to_go_test_() ->
+  D = autoattack:new(0, 5),
+  [
+    ?_assertEqual(0, autoattack:get_time_to_go(D))
+  ].
+
+get_target_test_() ->
+  D = autoattack:new(0, 5),
+  [
+    ?_assertEqual(undefined, autoattack:get_target(D))
+  ].
+
+set_target_test_() ->
+  D = autoattack:new(0, 5),
+  D2 = autoattack:set_target(0, D),
+  D3 = autoattack:set_target(undefined, D),
+  [
+    ?_assertEqual(undefined, autoattack:get_target(D)),
+    ?_assertEqual(0, autoattack:get_target(D2)),
+    ?_assertEqual(undefined, autoattack:get_target(D3))
+  ].
+
 is_ready_test_() ->
   D = autoattack:new(0, 5),
   D2 = autoattack:activate_cooldown(D),
