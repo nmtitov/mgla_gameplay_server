aaaf906fcd5546b86ff13d675444e2a0ffb4a5e3
Nikita Titov <nmtitov@yandex.ru>
2018-03-18 21:32:17 +0200

    Refactor autoattack

--- src/autoattack/data/autoattack.erl
+++ src/autoattack/data/autoattack.erl
@@ -47,6 +47,7 @@
 
 %% API
 
+-spec new(Id :: id_server:id(), Cooldown :: number()) -> data().
 new(Id, Cooldown) -> #{
   id => Id,
   target => undefined,
@@ -54,56 +55,47 @@ new(Id, Cooldown) -> #{
   time_left => 0
 }.
 
+-spec get_id(D :: data()) -> id_server:id().
 get_id(#{id := X}) -> X.
 
+-spec get_target(D :: data()) -> id_server:id_opt().
 get_target(#{target := X}) -> X.
+
+-spec set_target(X :: id_server:id_opt(), D :: data()) -> data().
 set_target(X, D) ->
   D#{
     target := X
   }.
 
+-spec get_cooldown(D :: data()) -> number().
 get_cooldown(#{cooldown := X}) -> X.
 
+-spec get_time_left(D :: data()) -> number().
 get_time_left(#{time_left := X}) -> X.
+
+-spec set_time_left(X :: number(), D :: data()) -> data().
 set_time_left(X, D) ->
   D#{
     time_left := X
   }.
 
+-spec update(Dt :: number(), D :: data()) -> data().
 update(Dt, D) ->
   set_time_left(get_time_left(D) - Dt, D).
 
+-spec is_ready(D :: data()) -> boolean().
 is_ready(D) ->
   get_time_left(D) =< 0.
 
+-spec trigger_cooldown(D :: data()) -> data().
 trigger_cooldown(D) ->
   case is_ready(D) of false -> error(internal); _ -> ok end,
   Cooldown = get_cooldown(D),
   set_time_left(Cooldown, D).
 
+-spec create_event(D :: data()) -> game_event().
 create_event(D) ->
   Id = get_id(D),
   TargetId = get_target(D),
   case TargetId == undefined of true -> error(internal); _ -> ok end,
   #{type => autoattack, body => #{from => Id, to => TargetId, damage => 10}}.
-
-
-%% Spec
-
--spec new(Id :: id_server:id(), Cooldown :: number()) -> data().
-
--spec get_id(D :: data()) -> id_server:id().
-
--spec get_target(D :: data()) -> id_server:id_opt().
--spec set_target(X :: id_server:id_opt(), D :: data()) -> data().
-
--spec get_cooldown(D :: data()) -> number().
-
--spec get_time_left(D :: data()) -> number().
--spec set_time_left(X :: number(), D :: data()) -> data().
-
--spec update(Dt :: number(), D :: data()) -> data().
--spec is_ready(D :: data()) -> boolean().
--spec trigger_cooldown(D :: data()) -> data().
-
--spec create_event(D :: data()) -> game_event().
