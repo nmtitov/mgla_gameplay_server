0f1073fc776bd871be2dd217ba7c76e2c6f24241
Nikita Titov <nmtitov@yandex.ru>
2023-05-21 15:58:34 +0200

    Update point.erl

--- src/math/point.erl
+++ src/math/point.erl
@@ -6,29 +6,30 @@
 -type point() :: {number(), number()}.
 -export_type([point/0]).
 
+-spec
+is_point(P :: point()) -> boolean().
 is_point({X, Y}) when is_number(X), is_number(Y) -> true;
 is_point(_) -> false.
 
+-spec
+distance(A :: point(), B :: point()) -> float().
 distance({X1, Y1} = A, {X2, Y2} = B) ->
   case is_point(A) of false -> error(badarg); _ -> ok end,
   case is_point(B) of false -> error(badarg); _ -> ok end,
   math:sqrt(math:pow((X2 - X1), 2) + math:pow(Y2 - Y1, 2)).
 
+-spec
+translate(A :: point(), B :: point()) -> point().
 translate({X1, Y1} = A, {X2, Y2} = B) ->
   case is_point(A) of false -> error(badarg); _ -> ok end,
   case is_point(B) of false -> error(badarg); _ -> ok end,
   {X1 + X2, Y1 + Y2}.
 
+-spec
+pointToMap(A :: point()) -> #{x:= number(), y := number()}.
 pointToMap({X, Y} = A) ->
   case is_point(A) of false -> error(badarg); _ -> ok end,
   #{
     x => X,
     y => Y
   }.
-
-
-%% Spec
--spec is_point(P :: point()) -> boolean().
--spec distance(A :: point(), B :: point()) -> float().
--spec translate(A :: point(), B :: point()) -> point().
--spec pointToMap(A :: point()) -> #{x:= number(), y := number()}.
